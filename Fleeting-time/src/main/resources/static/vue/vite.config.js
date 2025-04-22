d210df500395c6858cb0427ab0a9f580751d17f1
yang <2801672379@qq.com>
2025-04-22 11:47:00 +0800

    Update vite.config.js

--- Fleeting-time/src/main/resources/static/vue/vite.config.js
+++ Fleeting-time/src/main/resources/static/vue/vite.config.js
@@ -3,11 +3,6 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
-import { fileURLToPath, URL } from 'node:url'
- 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
  
 // https://vite.dev/config/
 export default defineConfig({
