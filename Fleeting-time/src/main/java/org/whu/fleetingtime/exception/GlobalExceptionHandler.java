3d193340a64c96e710782d256ff2993ab4cf8269
raitocc <raitocc@qq.com>
2025-04-24 18:55:19 +0800

    修复了Result顺序不固定的问题

--- Fleeting-time/src/main/java/org/whu/fleetingtime/exception/GlobalExceptionHandler.java
+++ Fleeting-time/src/main/java/org/whu/fleetingtime/exception/GlobalExceptionHandler.java
@@ -4,38 +4,28 @@ import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.HashMap;
-import java.util.Map;
+import org.whu.fleetingtime.pojo.Result;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(RuntimeException.class)
-    public Map<String, Object> handleRuntime(RuntimeException e) {
-        return buildError(500, "internal server error");
+    public Result<Object> handleRuntime(RuntimeException e) {
+        return Result.failure(401, "Invalid token");
     }
 
     @ExceptionHandler(IllegalArgumentException.class)
-    public Map<String, Object> handleIllegalArgument(IllegalArgumentException e) {
-        return buildError(400, "invalid parameters");
+    public Result<Object> handleIllegalArgument(IllegalArgumentException e) {
+        return Result.failure(400, "invalid parameters");
     }
 
     @ExceptionHandler(ExpiredJwtException.class)
-    public Map<String, Object> handleExpiredJwt(ExpiredJwtException e) {
-        return buildError(401, "expired token");
+    public Result<Object> handleExpiredJwt(ExpiredJwtException e) {
+        return Result.failure(401, "expired token");
     }
 
     @ExceptionHandler(JwtException.class)
-    public Map<String, Object> handleJwt(JwtException e) {
-        return buildError(401, "invalid Token");
-    }
-
-    private Map<String, Object> buildError(int code, String msg) {
-        Map<String, Object> res = new HashMap<>();
-        res.put("code", code);
-        res.put("msg", msg);
-        res.put("data","failure");
-        return res;
+    public Result<Object> handleJwt(JwtException e) {
+        return Result.failure(401, "invalid token");
     }
 }
