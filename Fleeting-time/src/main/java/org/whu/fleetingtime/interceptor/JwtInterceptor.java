04a288430cfb1acaf8e5293e277feeb3e2ef197e
raitocc <raitocc@qq.com>
2025-04-24 17:41:46 +0800

    统一拦截器响应的格式

--- Fleeting-time/src/main/java/org/whu/fleetingtime/interceptor/JwtInterceptor.java
+++ Fleeting-time/src/main/java/org/whu/fleetingtime/interceptor/JwtInterceptor.java
@@ -1,25 +1,33 @@
 package org.whu.fleetingtime.interceptor;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.lang.NonNull;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.whu.fleetingtime.pojo.Result;
 
 @Component
 public class JwtInterceptor implements HandlerInterceptor {
     // 前置拦截方法
     @Override
     public boolean preHandle(HttpServletRequest request, @NonNull HttpServletResponse response, @NonNull Object handler) throws Exception {
-        // 获取请求头中的 token
+
         String token = request.getHeader("Authorization");
 
         if (token == null || token.isEmpty()) {
-            response.setContentType("text/html;charset = UTF-8");
-            response.setStatus(401);  // 未授权
-            response.getWriter().write("未提供Token");
+            response.setContentType("application/json;charset=UTF-8");
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED); // 401
+            // 构造统一返回结果
+            Result<Object> result = Result.failure(401, "未提供Token");
+            // 转成 JSON 返回
+            ObjectMapper mapper = new ObjectMapper();
+            String json = mapper.writeValueAsString(result);
+
+            response.getWriter().write(json);
             return false;
         }
         return true;
