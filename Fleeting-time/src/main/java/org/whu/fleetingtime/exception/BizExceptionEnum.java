eaf29ddfa7f7019ac252eb9ed9291686ea91da97
raitocc <raitocc@qq.com>
2025-04-25 16:50:52 +0800

    增加checkin打卡获取请求参数异常

--- Fleeting-time/src/main/java/org/whu/fleetingtime/exception/BizExceptionEnum.java
+++ Fleeting-time/src/main/java/org/whu/fleetingtime/exception/BizExceptionEnum.java
@@ -5,12 +5,14 @@ import lombok.Getter;
 public enum BizExceptionEnum {
 
     //====用户模块====
-    //用户名已存在
-//    USER_EXIST(10001, "用户名已存在"),
     //用户名或密码错误
     USER_PASSWORD_OR_USERNAME_ERROR(10002, "invalid username or password"),
     //用户名已存在
-    USERNAME_ALREADY_EXISTS(10003, "username already exists");
+    USERNAME_ALREADY_EXISTS(10003, "username already exists"),
+
+    //====checkin模块
+    //用户名已存在
+    INVALID_CHECKIN_PARAMETER(10004, "latitude, longitude, city or timestamp is null");
 
 
     @Getter
