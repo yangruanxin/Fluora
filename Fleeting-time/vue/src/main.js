6d479053656f26567d9bd14a72ba00682916f8d6
yang <2801672379@qq.com>
2025-04-25 16:52:37 +0800

    实现前后端接口，实现未登录与登录状态切换

--- Fleeting-time/vue/src/main.js
+++ Fleeting-time/vue/src/main.js
@@ -3,11 +3,14 @@ import ElementPlus from 'element-plus'
 import "element-plus/dist/index.css"
 
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from '@/router'
 
 
+const pinia = createPinia() // 创建 Pinia 实例
 const app=createApp(App);
 app.use(router);
 app.use(ElementPlus);
+app.use(pinia);
 app.mount('#app');
