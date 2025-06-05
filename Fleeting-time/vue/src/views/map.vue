2dd294a54b8ba26bcea33a9ae50f32f01766b547
yang <2801672379@qq.com>
2025-06-05 16:30:08 +0800

    改错

--- Fleeting-time/vue/src/views/map.vue
+++ Fleeting-time/vue/src/views/map.vue
@@ -44,6 +44,7 @@
             placeholder="记录一下你的感受吧..."
             style="width: 100%; height: 280px; resize: none; padding: 10px; box-sizing: border-box;"
           ></textarea>
+        </div>
       </div>
 
 
